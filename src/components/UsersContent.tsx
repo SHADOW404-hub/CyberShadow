8d3f597ed83e9671fee973bc39d30d48245ea0aa
SHADOW404-hub <raximovshahriyor351@gmail.com>
2026-06-13 12:26:18 +0500

    update

--- src/components/UsersContent.tsx
+++ src/components/UsersContent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from './supabase';
+import { supabase } from '../services/supabase';
 
 interface UserProfile {
   id: string;
